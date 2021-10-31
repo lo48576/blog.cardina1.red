62b2b19389c6e94e9e56ea3ddad051c896094a80
YOSHIOKA Takuma <lo48576@hard-wi.red>
2021-11-01 04:19:07 +0900

    Bump deps

--- bin/apply-patch.sh
+++ bin/apply-patch.sh
@@ -5,23 +5,23 @@ cd "$(dirname "$(readlink -f "$0")")/.."
 
 ################
 
-RACK_LIVERELOAD_PATH="$(realpath --relative-to=. "$(bundle info --path rack-livereload)")"
-echo "rack-livereload: ${RACK_LIVERELOAD_PATH}"
-
-patch -p0 <<__EOF__
-diff -Naur ${RACK_LIVERELOAD_PATH}/lib/rack/livereload/processing_skip_analyzer.rb rack-livereload-0.3.17/lib/rack/livereload/processing_skip_analyzer.rb
---- ${RACK_LIVERELOAD_PATH}/lib/rack/livereload/processing_skip_analyzer.rb 2019-11-29 19:20:38.213601705 +0900
-+++ ${RACK_LIVERELOAD_PATH}/lib/rack/livereload/processing_skip_analyzer.rb      2019-11-29 22:43:58.691183891 +0900
-@@ -37,7 +37,7 @@
-       end
-
-       def html?
--        @headers['Content-Type'] =~ %r{text/html}
-+        @headers['Content-Type'] =~ %r{text/html|application/xhtml\+xml}
-       end
-
-       def get?
-__EOF__
+#RACK_LIVERELOAD_PATH="$(realpath --relative-to=. "$(bundle info --path rack-livereload)")"
+#echo "rack-livereload: ${RACK_LIVERELOAD_PATH}"
+#
+#patch -p0 <<__EOF__
+#diff -Naur ${RACK_LIVERELOAD_PATH}/lib/rack/livereload/processing_skip_analyzer.rb rack-livereload-0.3.17/lib/rack/livereload/processing_skip_analyzer.rb
+#--- ${RACK_LIVERELOAD_PATH}/lib/rack/livereload/processing_skip_analyzer.rb 2019-11-29 19:20:38.213601705 +0900
+#+++ ${RACK_LIVERELOAD_PATH}/lib/rack/livereload/processing_skip_analyzer.rb      2019-11-29 22:43:58.691183891 +0900
+#@@ -37,7 +37,7 @@
+#	   end
+#
+#	   def html?
+#-        @headers['Content-Type'] =~ %r{text/html}
+#+        @headers['Content-Type'] =~ %r{text/html|application/xhtml\+xml}
+#	   end
+#
+#	   def get?
+#__EOF__
 
 ################
 
